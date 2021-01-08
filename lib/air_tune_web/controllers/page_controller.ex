b852f19ba08e493b60823893c306b0ade29d162f
an-drian <a.poberezhnyk@gmail.com>
2021-01-08 17:21:31 +0200

    get 10 best stations

--- lib/air_tune_web/controllers/page_controller.ex
+++ lib/air_tune_web/controllers/page_controller.ex
@@ -1,7 +1,18 @@
 defmodule AirTuneWeb.PageController do
   use AirTuneWeb, :controller
 
+  import Ecto.Query
+  alias AirTune.Radio.Station
+
+  @repo AirTune.Repo
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    query = from s in Station,
+      select: %{name: s.name, homepage: s.homepage, clickcount: s.clickcount},
+      order_by: [desc: :clickcount],
+      limit: 10
+    stations = @repo.all(query)
+    IO.inspect("STACKs")
+    render(conn, "index.html", stations: stations)
   end
 end
